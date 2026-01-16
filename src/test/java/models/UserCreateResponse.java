d8a62b38132152cb55d390bcd07fe757f210e188
kettriin <e.shch1904@gmail.com>
2026-01-16 22:48:09 +0300

    reqres-api-tests: - исправила версию ломбок в грэдл - добавила в можели игнорирование неизвестных полей

--- src/test/java/models/UserCreateResponse.java
+++ src/test/java/models/UserCreateResponse.java
@@ -1,8 +1,10 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserCreateResponse {
     private String name;
     private String job;
--- src/test/java/models/UserUpdateResponse.java
+++ src/test/java/models/UserUpdateResponse.java
@@ -1,8 +1,10 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserUpdateResponse {
     private String name;
     private String job;
